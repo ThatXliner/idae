4319f159abffb69a5339fe763a75d7e1bf87a227
ThatXliner <bryan.hu.2020@gmail.com>
2023-08-26 08:33:43 -0700

    :rotating_light: Final linter silencing

--- tests/example.py
+++ tests/example.py
@@ -10,6 +10,6 @@ dependencies = [
 import requests
 from rich.pretty import pprint
 
-resp = requests.get("https://peps.python.org/api/peps.json")
+resp = requests.get("https://peps.python.org/api/peps.json")  # noqa: S113
 data = resp.json()
 pprint([(k, v["title"]) for k, v in data.items()][:10])
