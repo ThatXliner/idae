7a3b4b18cc49d948c8df8f2037e5e23b0b37f477
ThatXliner <bryan.hu.2020@gmail.com>
2023-08-26 10:39:27 -0700

    Oh

--- tests/example.py
+++ tests/example.py
@@ -1,11 +1,15 @@
 # /// pyproject
+# [run]
+# requires-python = ">=3.11"
+# dependencies = [
 #   "requests<3",
 #   "rich",
+# ]
 # ///
 
 import requests
 from rich.pretty import pprint
 
-resp = requests.get("https://peps.python.org/api/peps.json")  # noqa: S113
+resp = requests.get("https://peps.python.org/api/peps.json")  # noqa:
 data = resp.json()
 pprint([(k, v["title"]) for k, v in data.items()][:10])
